d210baf94f07127619c2de8b6160c24fcecd18da
drag <drag0n1zed@outlook.com>
2025-12-15 20:27:05 +0800

    Replace from_ and to_ functions with From traits

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -1,13 +1,4 @@
-// The F register (or flags register) is 8 bits, but only the top 4 are used.
-
-// Bit  Name  Set If...
-// 7    Z     Result of operation is Zero.
-// 6    N     The last operation was a Subtraction.
-// 5    H     There was a carry from bit 3 to bit 4.
-// 4    C     There was a carry from bit 7.
-// 3-0        Not used (always read as 0).
-
-#[derive(Default)]
+r#[derive(Default, Copy, Clone)]
 pub struct FlagsRegister {
     pub zero: bool,
     pub subtract: bool,
@@ -15,20 +6,28 @@ pub struct FlagsRegister {
     pub carry: bool,
 }
 
-impl FlagsRegister {
-    pub fn to_u8(&self) -> u8 {
-        (if self.zero { 1 } else { 0 }) << 7
-            | (if self.subtract { 1 } else { 0 }) << 6
-            | (if self.half_carry { 1 } else { 0 }) << 5
-            | (if self.carry { 1 } else { 0 }) << 4
-    }
+// Only the top 4 bits are used.
+// Bit  Name  Set If...
+// 7    Z     Result of operation is Zero.
+// 6    N     The last operation was a Subtraction.
+// 5    H     There was a carry from bit 3 to bit 4.
+// 4    C     There was a carry from bit 7.
 
-    pub fn from_u8(value: u8) -> Self {
+impl From<FlagsRegister> for u8 {
+    fn from(flags: FlagsRegister) -> u8 {
+        (if flags.zero { 1 } else { 0 }) << 7
+            | (if flags.subtract { 1 } else { 0 }) << 6
+            | (if flags.half_carry { 1 } else { 0 }) << 5
+            | (if flags.carry { 1 } else { 0 }) << 4
+    }
+}
+impl From<u8> for FlagsRegister {
+    fn from(byte: u8) -> FlagsRegister {
         FlagsRegister {
-            zero: (value >> 7) & 1 != 0,
-            subtract: (value >> 6) & 1 != 0,
-            half_carry: (value >> 5) & 1 != 0,
-            carry: (value >> 4) & 1 != 0,
+            zero: byte & 0b1000_0000 != 0,
+            subtract: byte & 0b0100_0000 != 0,
+            half_carry: byte & 0b0010_0000 != 0,
+            carry: byte & 0b0001_0000 != 0,
         }
     }
 }
@@ -54,12 +53,13 @@ impl Registers {
     }
 
     pub fn get_af(&self) -> u16 {
-        ((self.a as u16) << 8) | (self.f.to_u8() as u16)
+        let flags: u8 = self.f.into();
+        ((self.a as u16) << 8) | flags as u16
     }
 
     pub fn set_af(&mut self, value: u16) {
         self.a = ((value & 0xFF00) >> 8) as u8;
-        self.f = FlagsRegister::from_u8((value & 0x00F0) as u8);
+        self.f = ((value & 0x00F0) as u8).into();
     }
 
     pub fn get_bc(&self) -> u16 {
