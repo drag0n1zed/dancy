ef26860148c116dc8b25200d2e944fab5642e8a9
drag <drag0n1zed@outlook.com>
2025-12-16 23:51:06 +0800

    Implement instruction target read/write helper functions for CPU

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,7 +1,7 @@
 mod instructions;
 mod registers;
 
-use crate::cpu::instructions::{Instruction};
+use crate::cpu::instructions::{ByteDest, ByteSource, Instruction, WordDest, WordSource};
 use crate::cpu::registers::Registers;
 use crate::mmu::Bus;
 pub struct Cpu {
@@ -9,7 +9,7 @@ pub struct Cpu {
     pc: u16,
     sp: u16,
     ime: bool,
-    ime_scheduled: bool, 
+    ime_scheduled: bool,
     halted: bool,
 }
 
@@ -44,6 +44,99 @@ impl Cpu {
             // control/misc
             Instruction::NOP => 4,
             _ => unimplemented!(),
+        }
+    }
+
+    // Could modify registers.
+    fn resolve_byte_target(&mut self, bus: &Bus, source: ByteSource) -> u8 {
+        match source {
+            ByteSource::A => self.registers.a,
+            ByteSource::B => self.registers.b,
+            ByteSource::C => self.registers.c,
+            ByteSource::D => self.registers.d,
+            ByteSource::E => self.registers.e,
+            ByteSource::H => self.registers.h,
+            ByteSource::L => self.registers.l,
+
+            ByteSource::IndBC => bus.read(self.registers.get_bc()),
+            ByteSource::IndDE => bus.read(self.registers.get_de()),
+            ByteSource::IndHL => bus.read(self.registers.get_hl()),
+            ByteSource::IndHLI => {
+                let addr = self.registers.get_hl();
+                self.registers.set_hl(addr.wrapping_add(1));
+                bus.read(addr)
+            }
+            ByteSource::IndHLD => {
+                let addr = self.registers.get_hl();
+                self.registers.set_hl(addr.wrapping_sub(1));
+                bus.read(addr)
+            }
+
+            ByteSource::FF00PlusC => bus.read(0xFF00 | self.registers.c as u16),
+
+            ByteSource::Address(word) => bus.read(word),
+            ByteSource::Immediate(byte) => byte,
+        }
+    }
+
+    fn write_byte_target(&mut self, bus: &mut Bus, dest: ByteDest, value: u8) {
+        match dest {
+            ByteDest::A => self.registers.a = value,
+            ByteDest::B => self.registers.b = value,
+            ByteDest::C => self.registers.c = value,
+            ByteDest::D => self.registers.d = value,
+            ByteDest::E => self.registers.e = value,
+            ByteDest::H => self.registers.h = value,
+            ByteDest::L => self.registers.l = value,
+
+            ByteDest::IndBC => bus.write(self.registers.get_bc(), value),
+            ByteDest::IndDE => bus.write(self.registers.get_de(), value),
+            ByteDest::IndHL => bus.write(self.registers.get_hl(), value),
+            ByteDest::IndHLI => {
+                let addr = self.registers.get_hl();
+                self.registers.set_hl(addr.wrapping_add(1));
+                bus.write(addr, value)
+            }
+            ByteDest::IndHLD => {
+                let addr = self.registers.get_hl();
+                self.registers.set_hl(addr.wrapping_sub(1));
+                bus.write(addr, value)
+            }
+
+            ByteDest::FF00PlusC => bus.write(0xFF00 | self.registers.c as u16, value),
+
+            ByteDest::Address(word) => bus.write(word, value),
+        }
+    }
+
+    fn resolve_word_target(&mut self, source: WordSource) -> u16 {
+        match source {
+            WordSource::AF => self.registers.get_af(),
+            WordSource::BC => self.registers.get_bc(),
+            WordSource::DE => self.registers.get_de(),
+            WordSource::HL => self.registers.get_hl(),
+
+            WordSource::SP => self.sp,
+
+            WordSource::Immediate(word) => word,
+
+            // convert negative i8 to large u16. wrap sub value = wrap add (max - value)
+            WordSource::SPPlusImmediate(byte) => self.sp.wrapping_add(byte as u16),
+        }
+    }
+
+    fn write_word_target(&mut self, bus: &mut Bus, dest: WordDest, value: u16) {
+        match dest {
+            WordDest::AF => self.registers.set_af(value),
+            WordDest::BC => self.registers.set_bc(value),
+            WordDest::DE => self.registers.set_de(value),
+            WordDest::HL => self.registers.set_hl(value),
+
+            WordDest::SP => self.sp = value,
+
+            WordDest::Address(word) => {
+                bus.write_u16(word, value);
+            }
         }
     }
 }
